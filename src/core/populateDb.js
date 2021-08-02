30a5efe7286730eb4dc38b83feff82dd87461355
PaulSebalu <paolossebalu@gmail.com>
2021-08-02 16:24:50 +0300

    Chore: Add initial data loading configuration

--- src/core/populateDb.js
+++ src/core/populateDb.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 import acronyms from './acronym.json';
+import pool from './dbPool';
+import errorCodes from './pgErrorCodes';
+
+const populateData = () => {
+  acronyms.forEach((acronym) => {
+    const params = Object.entries(acronym).flat();
+
+    const sql = `INSERT INTO acronyms
+    (acronym, definition) 
+      VALUES ($1, $2) RETURNING *`;
+
+    pool.query(sql, params, (err, res) => {
+      if (err) {
+        const message = err.message.replace(/['"]+/g, '');
+        const pgErrorCode = errorCodes[err.code];
+        console.log(`Error:${message}`);
+        console.log(`pgErrorCode:${pgErrorCode}`);
+      }
+      if (res) console.log(`${res.rows[0].acronym} added to database`);
+    });
+  });
+};
+
+populateData();
