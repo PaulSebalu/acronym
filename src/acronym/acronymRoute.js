6397f1d49d3aa8e1086af8903de93992b2124eb2
PaulSebalu <paolossebalu@gmail.com>
2021-08-13 16:23:52 +0300

    Chore: Add create, update and delete endpoint routes

--- src/acronym/acronymRoute.js
+++ src/acronym/acronymRoute.js
@@ -1,8 +1,19 @@
 import express from 'express';
-import { acronyms } from './acronymController';
+import {
+  acronyms,
+  createAcronym,
+  updateAcronym,
+  deleteAcronym,
+} from './acronymController';
 
 const acronymRouter = express.Router();
 
 acronymRouter.get('/acronym', acronyms);
+
+acronymRouter.post('/acronym', createAcronym);
+
+acronymRouter.put('/acronym/:acronym', updateAcronym);
+
+acronymRouter.delete('/acronym/:acronym', deleteAcronym);
 
 export default acronymRouter;
